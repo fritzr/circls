fc85e9cb71c9e8df473ca2cc98f1ae517a6ba78d
Fritz Reese <Reese-Fritz@zai.com>
2016-11-20 22:23:01 -0500

    Update permissions handling for android 6+

--- CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/MainActivity.java
+++ CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.gmu.cs.CirclsClient;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -32,8 +36,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private PatternAdapter       patternAdapter;
     private CameraBridgeViewBase mOpenCvCameraView;
 
+    private static final int CAMERA_PERMS = 0xbeef;
+    protected boolean haveCamera = false;
+
+    private static final int IR_PERMS = 0xbadd;
+    protected boolean haveIR = false;
+
     static {
         System.loadLibrary("native-lib");
+    }
+
+    protected void getPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_CONTACTS)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.TRANSMIT_IR,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                        CAMERA_PERMS);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    haveCamera = true;
+                    // setup camera
+                    mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.surface_view);
+                    mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+                    mOpenCvCameraView.setCvCameraViewListener(this);
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    haveCamera = false;
+                }
+                return;
+            }
+            case IR_PERMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    haveIR = true;
+
+                    // setup InfraRed
+                    infraRed = new InfraRed(this, log);
+                    TransmitterType transmitterType = infraRed.detect();
+                    log.log("TransmitterType: " + transmitterType);
+                    infraRed.createTransmitter(transmitterType);
+                    patternAdapter = new PatternAdapter(log, transmitterType);
+                } else {
+                    haveIR = false;
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
@@ -48,17 +135,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText console = (EditText) this.findViewById(R.id.console);
         log = new LogToEditText(console, "CIRCLS");
 
-        // setup InfraRed
-        infraRed = new InfraRed(this, log);
-        TransmitterType transmitterType = infraRed.detect();
-        log.log("TransmitterType: " + transmitterType);
-        infraRed.createTransmitter(transmitterType);
-        patternAdapter = new PatternAdapter(log, transmitterType);
-
-        // setup camera
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.surface_view);
-        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
+        getPermissions ();
     }
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -80,7 +157,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
-        infraRed.start();
+        if (infraRed != null && haveIR)
+            infraRed.start();
+        if (!haveCamera)
+            return;
 
         if (!OpenCVLoader.initDebug()) {
             log.log("OpenCV: " + "Internal OpenCV library not found. Using OpenCV Manager for initialization");
@@ -99,6 +179,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Snackbar.make(v, "Sending IR...", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
+        if (infraRed != null && haveIR)
         infraRed.transmit(patternAdapter.createTransmitInfo(
                 new PatternConverter(PatternType.Cycles, 38000,494,114,38,38,38,38,38,38,38,38,38,38,38,114,38,1)));
     }
@@ -106,7 +187,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        infraRed.stop();
+        if (infraRed != null)
+            infraRed.stop();
 
         if (mOpenCvCameraView != null)
             mOpenCvCameraView.disableView();
