b8803c0b15f480750cbda435c9d4e282037146ff
mcaldwelva <mcaldwelva@gmail.com>
2016-12-04 00:57:52 -0500

    change from RGB to Lab colorspace

--- CirclsClient/app/src/main/cpp/native-lib.cpp
+++ CirclsClient/app/src/main/cpp/native-lib.cpp
@@ -1,28 +1,32 @@
 #include <jni.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 
 using namespace std;
 using namespace cv;
 
 extern "C"
-jintArray Java_edu_gmu_cs_CirclsClient_MainActivity_ImageProcessor(JNIEnv &env, jobject, Mat &mat) {
-    jint rgb[] = {0, 0, 0};
+jintArray Java_edu_gmu_cs_CirclsClient_MainActivity_ImageProcessor(JNIEnv &env, jobject, Mat &input) {
+    jint lab[] = {0, 0, 0};
 
-    unsigned long pixels = mat.total();
-    uchar *data = mat.data;
+    Mat output;
+    cvtColor(input, output, CV_RGB2Lab);
+
+    unsigned long pixels = output.total();
+    uchar *data = output.data;
     for (long i = 0; i < pixels; i++) {
-        rgb[0] += (*data++);
-        rgb[1] += (*data++);
-        rgb[2] += (*data++);
-        data++;
+        lab[0] += (*data++);
+        lab[1] += (*data++);
+        lab[2] += (*data++);
     }
 
-    rgb[0] /= pixels;
-    rgb[1] /= pixels;
-    rgb[2] /= pixels;
+    lab[0] /= pixels;
+    lab[1] /= pixels;
+    lab[2] /= pixels;
 
     jintArray ret = env.NewIntArray(3);
     if (ret != NULL) {
-        env.SetIntArrayRegion(ret, 0, 3, rgb);    }
+        env.SetIntArrayRegion(ret, 0, 3, lab);    }
     return ret;
 }
