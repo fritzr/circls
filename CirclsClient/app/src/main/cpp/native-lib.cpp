45b50dd985c882de75c04fa4931f80d321608d62
mcaldwelva <mcaldwelva@gmail.com>
2016-12-04 02:35:37 -0500

    Avg cols like ColorBars, display one row on "Scan" tap

--- CirclsClient/app/src/main/cpp/native-lib.cpp
+++ CirclsClient/app/src/main/cpp/native-lib.cpp
@@ -7,26 +7,35 @@ using namespace std;
 using namespace cv;
 
 extern "C"
-jintArray Java_edu_gmu_cs_CirclsClient_MainActivity_ImageProcessor(JNIEnv &env, jobject, Mat &input) {
-    jint lab[] = {0, 0, 0};
+jintArray Java_edu_gmu_cs_CirclsClient_MainActivity_ImageProcessor(JNIEnv &env, jobject, Mat &matRGB) {
+    Mat matLab;
+    cvtColor(matRGB, matLab, CV_RGB2Lab);
 
-    Mat output;
-    cvtColor(input, output, CV_RGB2Lab);
+    int rows = matLab.rows;
+    int cols = matLab.cols;
 
-    unsigned long pixels = output.total();
-    uchar *data = output.data;
-    for (long i = 0; i < pixels; i++) {
-        lab[0] += (*data++);
-        lab[1] += (*data++);
-        lab[2] += (*data++);
+    jint flat[cols][2];
+    memset(flat, 0, sizeof(flat));
+
+    // calculate total ab values for each column
+    uchar *data = matLab.data;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            data++; // skip L
+            flat[j][0] += (*data++);
+            flat[j][1] += (*data++);
+        }
     }
 
-    lab[0] /= pixels;
-    lab[1] /= pixels;
-    lab[2] /= pixels;
+    // average the ab values for each column
+    for (int j = 0; j < cols; j++) {
+        flat[j][0] /= rows;
+        flat[j][1] /= rows;
+    }
 
-    jintArray ret = env.NewIntArray(3);
+    jintArray ret = env.NewIntArray(cols * 2);
     if (ret != NULL) {
-        env.SetIntArrayRegion(ret, 0, 3, lab);    }
+        env.SetIntArrayRegion(ret, 0, cols * 2, (jint*) flat);
+    }
     return ret;
 }
