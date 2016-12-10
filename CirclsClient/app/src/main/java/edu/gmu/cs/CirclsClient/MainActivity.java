e792ddcf37a307c55a953646e0c5a4e0e0d29d41
mcaldwelva <mcaldwelva@gmail.com>
2016-12-10 03:04:58 -0500

    disable excessive OpenCV logging

--- CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/MainActivity.java
+++ CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
