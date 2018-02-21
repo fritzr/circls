36ba46ca2523e40ae6660167efba76486a4731c0
mcaldwelva <mcaldwelva@gmail.com>
2018-02-21 18:00:32 -0500

    changed to Camera2/OpenGL API for higher res

--- CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/RxHandler.java
+++ CirclsClient/app/src/main/java/edu/gmu/cs/CirclsClient/RxHandler.java
@@ -4,62 +4,26 @@ import android.util.Log;
 import android.view.View;
 
 import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraGLSurfaceView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-
-public class RxHandler implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class RxHandler implements CameraGLSurfaceView.CameraTextureListener {
     private static final String TAG = "RxHandler";
-    private static final int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
-    private static final long THREAD_KEEPALIVE = 1000; // ms
 
-    private final ThreadPoolExecutor mWorkers;
-    private CameraBridgeViewBase mCameraView;
     private BaseLoaderCallback mLoaderCallback;
     private MessageHandler mDisplay;
+    private CameraGLSurfaceView mView;
 
     // jni
     static { System.loadLibrary("native-lib"); }
-    private native char[] FrameProcessor(long inputFrame);
+    private native char[] FrameProcessor(int width, int height);
 
-
-    class Worker implements Runnable {
-        private Mat mat;
-        Worker(Mat mat) { this.mat = mat; }
-
-        @Override
-        public void run() {
-            char[] data = FrameProcessor(mat.getNativeObjAddr());
-
-            if (data.length > 0) {
-                mDisplay.update(data[0], String.valueOf(data));
-            }
-        }
-    }
-
-    public RxHandler() {
-        // only handle one frame at a time
-        mWorkers = new ThreadPoolExecutor(
-                1,
-                1,
-                THREAD_KEEPALIVE,
-                TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>()
-        );
-    }
 
     public void setup(View view, MessageHandler display) {
         // setup display
-        mCameraView = (CameraBridgeViewBase) view;
-        mCameraView.setMaxFrameSize(1920, 1080);
-        mCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
-        mCameraView.setCvCameraViewListener(this);
-        mCameraView.enableFpsMeter();
+        mView = (CameraGLSurfaceView) view;
+        mView.setCameraTextureListener(this);
 
         // setup callback for OpenCV loader
         mLoaderCallback = new BaseLoaderCallback(view.getContext()) {
@@ -68,9 +32,6 @@ public class RxHandler implements CameraBridgeViewBase.CvCameraViewListener2 {
                 switch (status) {
                     case LoaderCallbackInterface.SUCCESS:
                         Log.d(TAG, "OpenCV loaded successfully");
-                        if (mCameraView != null) {
-                            mCameraView.enableView();
-                        }
                         break;
                     default:
                         super.onManagerConnected(status);
@@ -83,15 +44,14 @@ public class RxHandler implements CameraBridgeViewBase.CvCameraViewListener2 {
     }
 
     public void start() {
-        if (mCameraView != null && OpenCVLoader.initDebug()) {
+        if (OpenCVLoader.initDebug()) {
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            mView.enableView();
         }
     }
 
     public void stop() {
-        if (mCameraView != null) {
-            mCameraView.disableView();
-        }
+        mView.disableView();
     }
 
     @Override
@@ -101,14 +61,16 @@ public class RxHandler implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     @Override
     public void onCameraViewStopped() {
-        mWorkers.purge();
     }
 
-    // queue up each frame for processing
     @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat mat = inputFrame.rgba();
-        mWorkers.execute(new Worker(mat));
-        return mat;
+    public boolean onCameraTexture(int texIn, int texOut, int width, int height) {
+        char[] data = FrameProcessor(width, height);
+        if (data.length > 0) {
+            mDisplay.update(data[0], String.valueOf(data));
+        }
+
+        // output isn't modified
+        return false;
     }
 }
